5be5ef2116ae2c7aef3e259818b9d7588e66b3d6
krratul11 <ratulmalik796@gmail.com>
2025-01-04 23:49:58 +0600

    Update task_01.php

--- task_01.php
+++ task_01.php
@@ -12,8 +12,3 @@ print_r($upperCaseArray);
 
 ?>
 
- README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/krratul11/live-test.git
-git push -u origin main
